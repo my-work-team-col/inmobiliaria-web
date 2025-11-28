f23bb9e327137650536293acf801f89466af3515
Didier Mendez <didierm.com@gmail.com>
2025-11-28 17:18:27 -0500

    feat(content) add new pages and layout

---
+++ src/types/listing.ts
@@ -0,0 +1,44 @@
+export interface TopLevel {
+    id:            number;
+    title:         string;
+    slug:          string;
+    categories:    Category[];
+    isActive:      boolean;
+    featured:      boolean;
+    gallery:       Gallery[];
+    location:      Location;
+    city:          City;
+    neighborhood:  string;
+    code:          string;
+    description:   string;
+    area:          number;
+    bedrooms:      number;
+    bathrooms:     number;
+    parking:       number;
+    price:         number;
+    participation: Participation;
+    address:       string;
+    observations:  string;
+}
+
+export enum Category {
+    Apartamento = "apartamento",
+    Casa = "casa",
+    Venta = "venta",
+}
+
+export enum City {
+    Bogotá = "Bogotá",
+}
+
+export enum Gallery {
+    ImagesProperty2Jpg = "/images/property-2.jpg",
+}
+
+export enum Location {
+    BogotáColombia = "Bogotá, Colombia",
+}
+
+export enum Participation {
+    The100 = "100%",
+}
